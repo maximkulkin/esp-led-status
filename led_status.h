c56094c4c5bb92e33f7c019d548171bbe0497c36
Maxim Kulkin <maxim.kulkin@gmail.com>
2019-02-02 06:49:21 -0800

    Fix usage of led_status_t in APIs

--- led_status.h
+++ led_status.h
@@ -4,7 +4,7 @@
 
 typedef void * led_status_t;
 
-led_status_t *led_status_init(int gpio);
-void led_status_done(led_status_t *status);
+led_status_t led_status_init(int gpio);
+void led_status_done(led_status_t status);
 
-void led_status_set(led_status_t *status, led_status_pattern_t *pattern);
+void led_status_set(led_status_t status, led_status_pattern_t *pattern);
